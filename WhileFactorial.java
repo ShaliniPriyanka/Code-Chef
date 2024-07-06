a0fa9ae8130ea128daeb8446ac6c0854eb75c11d
ShaliniPriyanka <21ec75@kcgcollege.com>
2024-07-06 21:07:34 +0530

    Factorial

---
+++ WhileFactorial.java
@@ -0,0 +1,33 @@
+/*Coding problem - 2
+Chef's coding journey continues.
+Chef was given an integer input 
+𝑁
+N.
+
+He wrote a code using WHILE loops to output the factorial of N.
+But he wasn't able to complete some parts of the code.
+Help him complete his code.
+
+Note: The factorial of a number 
+𝑁
+N is the product of each number from 1 to 
+𝑁
+N.
+𝑁
+!
+N!(N factorial)
+=
+1
+×
+2
+×
+3
+×
+.
+.
+.
+.
+.
+𝑁
+=1×2×3×.....N
+*/
