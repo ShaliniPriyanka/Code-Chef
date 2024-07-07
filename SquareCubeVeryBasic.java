73dc4cadcfca63cc3da4d76382ec987259c7b1e6
ShaliniPriyanka <21ec75@kcgcollege.com>
2024-07-07 13:27:59 +0530

    Code

---
+++ SquareCubeVeryBasic.java
@@ -0,0 +1,29 @@
+/*
+ Why we need method
+Write a program to calculate the square and cube of numbers 2, 3, and 4.
+
+Output:
+4
+8
+9
+27
+16
+64
+ */
+
+class SquareCubeVeryBasic {
+    public static void main (String[] args)
+	{
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        
+        System.out.println(a*a);
+        System.out.println(a*a*a);
+        System.out.println(b*b);
+        System.out.println(b*b*b);
+        System.out.println(c*c);
+        System.out.println(c*c*c);
+
+	}    
+}
