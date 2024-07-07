a8005e69524a16a199e799641609ff06acb64ae9
ShaliniPriyanka <21ec75@kcgcollege.com>
2024-07-07 12:55:43 +0530

    Code

--- ArrayFirstLast.java
+++ ArrayFirstLast.java
@@ -1,3 +1,24 @@
+/*
+ * Coding problem - 1
+You are given the following as a user input:
+
+First line contains the integer 𝑁.
+The second line contains N space separated integers.
+The third line contains and integer X.
+You need to insert 𝑋 as the 1st and last element and output the list.
+Your list will now have 
+𝑁+2 elements.
+Check the sample output for more details:
+Sample Input: 
+5
+1 2 3 4 5
+4
+
+Output: 
+4 1 2 3 4 5
+ */
+
+
 import java.util.Scanner;
 class ArrayFirstLast {
     public static void main(String[] args) {
